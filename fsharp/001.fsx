7b9ee955c40fb287a6ac4a1311c56c9e6713f39e
Emma Bahlke <emma.bahlke@edpsoftware.com>
2017-07-31 14:00:34 -0700

    Adding project 1 in F#

---
+++ fsharp/001.fsx
@@ -0,0 +1,11 @@
+// PROBLEM 1:
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+let isDivisibleByAny divisors number = List.filter (fun x -> number % x = 0) divisors |> List.length > 0
+let sum_multiples numbers cutoff = List.filter (fun x -> isDivisibleByAny numbers x) [1..cutoff] |> List.fold (fun acc num -> num + acc) 0
+
+let result = sum_multiples [3;5] 999
+
+// Dumb hardcoded version:
+let result = List.filter (fun x -> x % 3 = 0 || x % 5 = 0) [1..999] |> List.fold (fun acc num -> num + acc) 0
