484d69b17c700f511bda4dd7287dd9f33d3353c3
Emma Leonie <leonie@Pythia.local>
2017-08-24 12:53:43 -0700

    Cleanup on F# files

--- fsharp/005.fsx
+++ fsharp/005.fsx
@@ -1,3 +1,7 @@
+// PROBLEM 5:
+// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+// What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
 module Problem005
     let rec gcd a b =
         if b = 0 then a
--- fsharp/006.fsx
+++ fsharp/006.fsx
@@ -1,5 +1,15 @@
-let differenceBetweenSquareOfSumAndSumOfSquares numbers =
-    let square x = x*x
-    let sumOfSquares = List.sumBy square numbers
-    let squareOfSum = List.sum numbers |> square
-    squareOfSum - sumOfSquares
+// PROBLEM 6:
+// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+// Version 1 takes any list of numbers and operates in O(n) time
+module Problem006 =
+    let differenceBetweenSquareOfSumAndSumOfSquares numbers =
+        let square x = x*x
+        let sumOfSquares = List.sumBy square numbers
+        let squareOfSum = List.sum numbers |> square
+        squareOfSum - sumOfSquares
+
+// Version 2 only takes a range from 1..n but operates in O(1) time using closed-form expressions
+module Problem006ClosedForm =
+    let differenceBetweenSquareOfSumAndSumOfSquaresUpTo n =
+        ((n*(n+1))/2 * (n*(n+1))/2) - (n*(n+1)*(2*n+1))/6
