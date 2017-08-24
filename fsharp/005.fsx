cb4d87cb4cae14fc86246fea566592d454c7b0ee
Emma Leonie <leonie@Pythia.local>
2017-08-24 14:15:56 -0700

    Updating comments

--- fsharp/005.fsx
+++ fsharp/005.fsx
@@ -2,6 +2,9 @@
 // 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+// NOTE: The prime (') versions are added to play with bigints, and also because the original listlcm [1..20] lead to an overflow error
+// on the last term of the fold (since lcm (listlcm[1..19]) 20 tried to compute (listlcm[1..19])*20, which is 4655851200 and exceeds
+// the max value of a regular int in F#)
 module Problem005
     let rec gcd a b =
         if b = 0 then a
