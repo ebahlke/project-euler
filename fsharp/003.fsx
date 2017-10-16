b845f8939ba57bbe8c0ae209f0790e1e19142179
Emma Leonie <leonie@Pythia.local>
2017-10-16 13:32:05 -0700

    Updating 003

--- fsharp/003.fsx
+++ fsharp/003.fsx
@@ -3,24 +3,25 @@
 // What is the largest prime factor of the number 600851475143 ?
 
 module Problem003 =
+    // VERSION 1:
     // Attempted a Sieve of Eratosthenes-inspired version, which is sadly very non-performant after ~100000 and takes several minutes
-    // to find the right answer for the number necessary (floor of sqrt 600851475143.0)
-    let getPrimesUpTo n =
-        let listContainsNoDivisors list a =
-            not (List.exists (fun elem -> a%elem = 0) list)
-        List.fold (fun acc x -> if (listContainsNoDivisors acc x) then x::acc else acc) [] [2..n]
+    // to find the right answer for the number necessary (floor of sqrt 600851475143)
+    let getPrimesUpTo (n:int64) =
+        let listContainsNoDivisors (list:List<int64>) (a:int64) =
+            not (List.exists (fun elem -> a%elem = int64(0)) list)
+        List.fold (fun acc x -> if (listContainsNoDivisors acc x) then x::acc else acc) [] [int64(2)..n]
 
-    let getLargestPrimeFactor' (n:float) =
-        let potentialPrimeFactors = getPrimesUpTo ((sqrt >> int) n)
-        List.tryFind (fun elem -> n%(float elem) = 0.0) potentialPrimeFactors
+    let getLargestPrimeFactor' (n:int64) =
+        let potentialPrimeFactors = getPrimesUpTo n
+        List.tryFind (fun elem -> n%elem = int64(0)) potentialPrimeFactors
 
+    // VERSION 2:
+    // More or less trial division... also not very performant.
     // isPrime function taken from http://www.fssnip.net/3X
-    // This version is considerably more performant.
-    let isPrime n =
-        let sqrt' = (float >> sqrt >> int) n
-        [ 2 .. sqrt' ]
-        |> List.forall (fun x -> n % x <> 0)
+    let isPrime (n:int64) =
+        let sqrt' = (float >> sqrt >> int64) n
+        [ int64(2) .. sqrt' ]
+        |> List.forall (fun x -> n % x <> int64(0))
 
-    let getLargestPrimeFactor (n:float) =
-        let cutoff = (sqrt >> int) n
-        List.rev [1..cutoff] |> List.tryFind (fun elem -> isPrime elem && n%(float elem) = 0.0)
+    let getLargestPrimeFactor (n:int64) =
+        List.rev [int64(1)..n] |> List.tryFind (fun elem -> isPrime elem && n%elem = int64(0))
