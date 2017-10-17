8faed9e492d1c26018ed7c922565d4a4f0a2e5a0
Emma Leonie <leonie@Pythia.local>
2017-10-17 06:59:20 -0700

    Minor tweaks to problem 7

--- fsharp/007.fsx
+++ fsharp/007.fsx
@@ -2,8 +2,8 @@
 // By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 // What is the 10 001st prime number?
 // Decided to have some fun with sequences here.  Haven't really thought through performance, but it "only" takes 9-10 seconds to
-// get the correct answer for PE007, which is at least semi-reasonable.
-// It's not ideal - I haven't figured out yet how to use sequences to "skip"/continue rather than stopping -
+// get the correct answer for PE007, which is at least semi-reasonable.  However, there are a lot of inefficiencies with the algorithm.
+// So it's not ideal - I haven't figured out yet how to use sequences to "skip"/continue rather than stopping -
 // the ideal version would be something like:
 // Seq.unfold(fun (primesSoFar, nextNumberToCheck) ->
 //  if not any primesSoFar divide nextNumberToCheck then Some(nextNumberToCheck, (nextNumberToCheck::primesSoFar, nextNumberToCheck+1))
