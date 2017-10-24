c2d453cc8d45bd62a82235e5cd91c1ac710450f0
Emma Leonie <leonie@Pythia.local>
2017-10-23 19:58:44 -0700

    Minor cleanup on 019

--- fsharp/019.fsx
+++ fsharp/019.fsx
@@ -5,16 +5,14 @@
 
 module Problem019 =
     let isLeapYear n = ((n%100 <> 0) || (n%400 = 0)) && (n%4 = 0)
+    let monthGaps = [31; 28; 31; 30; 31; 30; 31; 31; 30; 31; 30; 31];
     
     (2, 1901)
     |>
     Seq.unfold(fun state ->
         if (snd state = 2001) then None
         else
-            let monthGaps =
-                if (isLeapYear (snd state))
-                    then [31; 29; 31; 30; 31; 30; 31; 31; 30; 31; 30; 31]
-                    else [31; 28; 31; 30; 31; 30; 31; 31; 30; 31; 30; 31]
+            let februaryLength = if (isLeapYear (snd state)) then 29 else 28
 
             let firstOfMonths =
                 (1, fst state)
@@ -22,7 +20,8 @@ module Problem019 =
                 Seq.unfold(fun (month, dayOfWeek) ->
                     if (month = 13) then None
                     else
-                        Some(dayOfWeek, (month+1, ((dayOfWeek + monthGaps.[month-1])%7))))
+                        let monthLength = if (month = 2) then februaryLength else monthGaps.[month-1]
+                        Some(dayOfWeek, (month+1, ((dayOfWeek + monthLength)%7))))
             let countOfSundays = firstOfMonths |> Seq.filter (fun day -> day = 0) |> Seq.length
             Some(countOfSundays, (((Seq.last firstOfMonths)+31)%7, snd state + 1)))
     |> Seq.sum
